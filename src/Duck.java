3d41003a0afb5644d421aedea9957a9c9eef7d9b
pratikbhagwat <pratikbhagwat4@gmail.com>
2020-05-10 00:39:28 -0400

    Initial commit

---
+++ src/Duck.java
@@ -0,0 +1,29 @@
+public abstract class Duck {
+    public FlyBehavior flyBehavior;
+    public QuackBehavior quackBehavior;
+
+    public Duck(){
+
+    }
+    public abstract void display();
+
+    public void performFly(){
+        flyBehavior.fly();
+    }
+    public void performQuack(){
+        quackBehavior.quack();
+    }
+
+    public void swim(){
+        System.out.println("All ducks swim the same way");
+    }
+
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
+
+}
