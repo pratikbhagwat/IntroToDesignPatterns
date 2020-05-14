b8e2a96d6d272a3508bd07a090ea53a14d3e8024
pratikbhagwat <pratikbhagwat4@gmail.com>
2020-05-14 06:23:08 -0400

    Factory Pattern (Pizza Example) added cali

--- src/PizzaStoreFactoryPattern/PizzaOrderingApp.java
+++ src/PizzaStoreFactoryPattern/PizzaOrderingApp.java
@@ -3,8 +3,12 @@ package PizzaStoreFactoryPattern;
 public class PizzaOrderingApp {
     public static void main(String[] args) {
         PizzaStore pizzaStore = new CaliPizzaStore("cheese");
-
         pizzaStore.orderPizza();
 
+        pizzaStore = new NyPizzaStore("cheese");
+        pizzaStore.orderPizza();
+
+        pizzaStore = new ChicagoPizzaStore("pepperoni");
+        pizzaStore.orderPizza();
     }
 }
